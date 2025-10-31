9a6894599dfc4af2379dc11e0a65a0478a923950
Jonathan Diez <65226124+JonathanDiez@users.noreply.github.com>
2025-10-31 01:38:54 +0100

    Add Firebase configuration file
    This file exports the Firebase configuration and loads the SDK for use in the application.

---
+++ js/firebase-config.js
@@ -0,0 +1,14 @@
+// js/firebase-config.js
+// Este archivo exporta la config y carga el SDK (se importa desde app.js)
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBmbQfw7KzEMaGvP3xft6g3vOTQpQkUjOE",
+  authDomain: "inventario-lm.firebaseapp.com",
+  projectId: "inventario-lm",
+  storageBucket: "inventario-lm.firebasestorage.app",
+  messagingSenderId: "286539412641",
+  appId: "1:286539412641:web:a20e6386a2b90fe2bcace0",
+  measurementId: ""
+};
+
+// No inicializamos Firebase aquí — app.js lo hará usando esta config.
+// Si por seguridad quieres reemplazar estos valores manualmente, hazlo aquí.
